5d45c7fa6da80c10a1985cbd7f17d0accad37e56
Tatiana Montenegro <tatiana.caldeira.tm@gmail.com>
2021-01-05 18:07:14 +0000

    Criar a base de dados e tabela produtos

---
+++ LojaShokuku/Dados/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using LojaShokuku.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaShokuku.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ LojaShokuku/Dados/Entidades/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaShokuku.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        //get; set; significa leitura e escrita
+        //aqui estão as propriedades, ou seja, os campos da tabela
+        //o entitity framework core é a tecnologia q usando c# me permite criar as bases de dados e tabelas e tudo oq fiz a bocado
+        //o entitity framework core mapeia isto e mete.o automatico e chave primaria
+
+        public string Nome { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Image")]
+        public string UrlDaImagem { get; set; }
+        //este é o caminho da imagem, nao onde fica a imagem
+
+        [Display(Name = "Última Compra")]
+        public DateTime UltimaCompra { get; set; }
+
+        [Display(Name = "Última Venda")]
+        public DateTime UltimaVenda { get; set; }
+
+        [Display(Name = "Disponível")]
+        public bool Disponivel { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
